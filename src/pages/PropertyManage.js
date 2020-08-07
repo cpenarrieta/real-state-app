daeb74bf5b53eee6865158293e09b4a282845043
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2020-08-07 16:15:49 -0700

    get Property

--- src/pages/PropertyManage.js
+++ src/pages/PropertyManage.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+
+const PROPERTY_QUERY = gql`
+  query GetProperty($uuid: String!) {
+    property(uuid: $uuid) {
+      uuid
+      title
+    }
+  }
+`;
 
 export default function PropertyManage() {
   const { propertyId } = useParams();
+  const { loading, error, data } = useQuery(PROPERTY_QUERY, {
+    variables: { uuid: propertyId },
+  });
 
-  return <div>Current Property: {propertyId}</div>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return <div>Current Property: {data?.property?.uuid}</div>;
 }
