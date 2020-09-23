456ee6c4c1d1a32d69382eed13793cf495e79ad4
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2020-09-22 23:07:11 -0700

    preview using component library

--- src/pages/ManagePropertyPreview.js
+++ src/pages/ManagePropertyPreview.js
@@ -39,6 +39,47 @@ const PROPERTY_QUERY = gql`
       hidePrice
       strata
     }
+    attachments(uuid: $uuid) {
+      id
+      title
+      url
+    }
+    me {
+      email
+      firstName
+      lastName
+      phone
+      address
+      address1
+      address2
+      city
+      province
+      zipCode
+      country
+      picture
+      pictureLowRes
+      username
+      smallBio
+    }
+    otherProperties(uuid: $uuid) {
+      title
+      uuid
+      bedrooms
+      bathrooms
+      price
+      mainPictureLowRes
+      currency
+      status
+      city
+    }
+    propertyImages(uuid: $uuid) {
+      id
+      order
+      title
+      description
+      url
+      urlLowRes
+    }
   }
 `;
 
@@ -51,7 +92,49 @@ export default function ManagePropertyPreview() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error...</p>;
 
-  const property = data?.property
+  const property = data?.property;
+  const attachments = data?.attachments;
+  const otherProperties = data?.otherProperties;
+  const images = data?.propertyImages;
+  const {
+    email: userEmail,
+    firstName: userFirstName,
+    lastName: userLastName,
+    phone: userPhone,
+    picture: userPicture,
+    pictureLowRes: userPictureLowRes,
+    address1: userAddress1,
+    address2: userAddress2,
+    city: userCity,
+    province: userProvince,
+    zipCode: userZipCode,
+    country: userCountry,
+    smallBio: userSmallBio,
+    username,
+  } = data?.me;
 
-  return <PropertyPage {...property} />;
+  return (
+    <PropertyPage
+      {...property}
+      attachments={attachments}
+      otherProperties={otherProperties}
+      images={images}
+      mapKey={process.env.REACT_APP_MAPS_KEY}
+      username={username}
+      visitorSource="preview"
+      userEmail={userEmail}
+      userFirstName={userFirstName}
+      userLastName={userLastName}
+      userPhone={userPhone}
+      userPicture={userPicture}
+      userPictureLowRes={userPictureLowRes}
+      userAddress1={userAddress1}
+      userAddress2={userAddress2}
+      userCity={userCity}
+      userProvince={userProvince}
+      userZipCode={userZipCode}
+      userCountry={userCountry}
+      userSmallBio={userSmallBio}
+    />
+  );
 }
