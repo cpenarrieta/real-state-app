708073d39d4194d18828c58b13a4158e4192ff39
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2020-09-27 23:01:16 -0700

    Settings page

---
+++ src/components/order/OrderItem.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { format, parseISO } from "date-fns";
+
+type OrderItemProps = {
+  receiptUrl: string;
+  createdAt: string;
+  amountTotal: number;
+  currency: string;
+  paid: boolean;
+  priceType: string;
+};
+
+export default function OrderItem({
+  receiptUrl,
+  createdAt,
+  amountTotal,
+  currency,
+  paid,
+  priceType,
+}: OrderItemProps) {
+  const dateFormat = format(parseISO(createdAt), "MMM do yyyy");
+
+  // TODO what to do in the rest of scenarios
+  if (!paid) return;
+
+  return (
+    <div className="mt-2">
+      <div className="rounded-md bg-gray-50 px-6 py-5 sm:flex sm:items-start sm:justify-between">
+        <div className="sm:flex sm:items-start">
+          <svg
+            className="h-8 w-auto sm:flex-shrink-0 sm:h-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+            />
+          </svg>
+
+          <div className="mt-3 sm:mt-0 sm:ml-4">
+            <div className="text-sm leading-5 font-medium text-gray-900">
+              ${amountTotal / 100}{" "}
+              <span className="uppercase text-gray-400">{currency}</span>
+            </div>
+            <div className="mt-1 text-sm leading-5 text-gray-600 sm:flex sm:items-center">
+              <div>Paid on {dateFormat}</div>
+              <span className="hidden sm:mx-2 sm:inline" aria-hidden="true">
+                &middot;
+              </span>
+              <div className="mt-1 sm:mt-0">1 {priceType} package</div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-4 sm:mt-0 sm:ml-6 sm:flex-shrink-0">
+          <span className="inline-flex rounded-md shadow-sm">
+            <a
+              className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+              href={receiptUrl}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Receipt
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/manageProperty/ManagePropertySettings.js
+++ src/pages/manageProperty/ManagePropertySettings.js
@@ -1,5 +1,95 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+import OrderItem from "../../components/order/OrderItem";
+
+const PROPERTY_ORDERS_QUERY = gql`
+  query PropertyOrders($uuid: String!) {
+    propertyOrders(uuid: $uuid) {
+      chargeId
+      createdAt
+      amountTotal
+      currency
+      paid
+      refunded
+      paymentType
+      priceType
+      receiptUrl
+    }
+  }
+`;
 
 export default function ManagePropertySettings() {
-  return <div>ManagePropertySettings</div>;
+  const { propertyId } = useParams();
+  const { loading, error, data } = useQuery(PROPERTY_ORDERS_QUERY, {
+    variables: { uuid: propertyId },
+  });
+
+  if (loading) {
+    return <p>loading...</p>;
+  }
+
+  const orders = data?.propertyOrders;
+
+  return (
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 border-b border-gray-200 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          Property Settings
+        </h3>
+      </div>
+      <div className="px-4 py-5 sm:p-0">
+        <dl>
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
+            <dt className="text-sm leading-5 font-medium text-gray-500">
+              Package
+            </dt>
+            <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+              One year
+            </dd>
+          </div>
+          <div className="mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5">
+            <dt className="text-sm leading-5 font-medium text-gray-500">
+              Valid until
+            </dt>
+            <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+              Oct 28th 2021
+            </dd>
+          </div>
+          <div className="mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5">
+            <dt className="text-sm leading-5 font-medium text-gray-500">
+              Billing History
+            </dt>
+            <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+              {orders.map((o) => (
+                <OrderItem
+                  key={o.chargeId}
+                  receiptUrl={o.receiptUrl}
+                  createdAt={o.createdAt}
+                  amountTotal={o.amountTotal}
+                  currency={o.currency}
+                  paid={o.paid}
+                  paymentType={o.paymentType}
+                  priceType={o.priceType}
+                />
+              ))}
+            </dd>
+          </div>
+          <div className="mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5">
+            <dt className="text-sm leading-5 font-medium text-gray-500">
+              Delete Property
+            </dt>
+            <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+              <button
+                type="button"
+                className="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-50 focus:outline-none focus:border-red-300 focus:shadow-outline-red active:bg-red-200 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+              >
+                Delete Property
+              </button>
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  );
 }
