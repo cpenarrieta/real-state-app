43970c84b08d365fe3e94c75aaeea60a2853ffe3
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2020-10-02 01:25:00 -0700

    done Onboarding

--- src/pages/onboarding/Summary.js
+++ src/pages/onboarding/Summary.js
@@ -42,7 +42,7 @@ export default function Summary({ readyForDone }) {
     <div className="bg-white">
       <div className="max-w-screen-xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-          You are ready to create your first Property! <span>🎉</span>
+          You are ready to create your first Property! <span role="img" aria-label="party">🎉</span>
         </h2>
         <div className="mt-8 flex justify-center">
           <div className="inline-flex rounded-md shadow">
