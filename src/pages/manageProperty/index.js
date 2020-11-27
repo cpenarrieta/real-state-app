2fde20558e21caebf58050e66b51655c8fb3d667
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2020-11-26 22:42:37 -0800

    fix edit menu

--- src/pages/manageProperty/index.js
+++ src/pages/manageProperty/index.js
@@ -17,8 +17,8 @@ import ManagePropertyPayment from "./ManagePropertyPayment";
 import { getPublishedStatus } from "../../util/propertyStatus";
 import ShareModal from "../../components/share/ShareModal";
 import { PROPERTY_QUERY } from "../../queries/getProperty";
-import Loading from '../../components/Loading'
-import Error from '../../components/Error'
+import Loading from "../../components/Loading";
+import Error from "../../components/Error";
 
 const ButtonNotSelected =
   "group inline-flex items-center py-4 px-1 border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300";
@@ -185,7 +185,7 @@ export default function ManageProperty() {
       <div className="pt-5">
         <div className="block">
           <div className="border-b border-gray-200">
-            <nav className="flex -mb-px">
+            <nav className="flex flex-wrap -mb-px">
               <button
                 className={`${
                   matchEdit || isRoot ? ButtonSelected : ButtonNotSelected
