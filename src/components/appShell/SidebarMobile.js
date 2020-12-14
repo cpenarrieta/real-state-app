bf0f5786d738282bab31fbf3459de22b28972ee8
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2020-12-14 01:05:44 -0800

    support page

--- src/components/appShell/SidebarMobile.js
+++ src/components/appShell/SidebarMobile.js
@@ -256,6 +256,31 @@ export default function SidebarMobile({
                     My Website
                   </a>
 
+                  <Link
+                    to="/support"
+                    className={`group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-600 rounded-md ${
+                      currentPath === "/support" ? "bg-gray-100" : ""
+                    } hover:text-logoFont hover:bg-gray-50 focus:outline-none focus:text-logoFont focus:bg-gray-${
+                      currentPath === "/support" ? "200" : "50"
+                    } transition ease-in-out duration-150`}
+                  >
+                    <svg
+                      className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
+                      />
+                    </svg>
+                    Support
+                  </Link>
+
                   <span className="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-600 rounded-md hover:text-logoFont hover:bg-gray-50 focus:outline-none focus:text-logoFont focus:bg-gray-100 transition ease-in-out duration-150">
                     <button
                       type="button"
