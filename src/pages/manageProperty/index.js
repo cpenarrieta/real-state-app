d430611f4e897cb0682a62bc597dfce3b1d7a5e1
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2021-03-08 23:44:58 -0800

    tooltip

--- src/pages/manageProperty/index.js
+++ src/pages/manageProperty/index.js
@@ -246,27 +246,36 @@ export default function ManageProperty() {
           )}
 
           {isPropertyActive && (
-            <a href={liveWebsiteUrl} rel="noopener noreferrer" target="_blank">
-              <span className="sm:block ml-3 shadow-sm rounded-md">
-                <button
-                  type="button"
-                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-logoRed hover:bg-logoRed-500 focus:outline-none focus:shadow-outline-logoRed focus:border-logoRed active:bg-logoRed transition duration-150 ease-in-out"
-                >
-                  <svg
-                    className="-ml-1 mr-2 h-5 w-5 text-white-500"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  Go to live Website
-                </button>
+            <div class="has-tooltip">
+              <span class="tooltip rounded shadow-lg p-1 bg-gray-100 text-logoFont -mt-8">
+                Updates may take up to 30'
               </span>
-            </a>
+              <a
+                href={liveWebsiteUrl}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <span className="sm:block ml-3 shadow-sm rounded-md">
+                  <button
+                    type="button"
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-logoRed hover:bg-logoRed-500 focus:outline-none focus:shadow-outline-logoRed focus:border-logoRed active:bg-logoRed transition duration-150 ease-in-out"
+                  >
+                    <svg
+                      className="-ml-1 mr-2 h-5 w-5 text-white-500"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    Go to live Website
+                  </button>
+                </span>
+              </a>
+            </div>
           )}
 
           {!isPropertyActive && !isPropertyValidForFreeTrial && (
